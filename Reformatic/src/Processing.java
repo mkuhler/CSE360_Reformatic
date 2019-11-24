7448024e9f0a81de3556d5c92b58cbdcfd19fcd9
Luis Prieto <46549710+LuPri@users.noreply.github.com>
2019-11-23 21:28:59 -0700

    Added flag checking and re-purposed sample code

--- Reformatic/src/Processing.java
+++ Reformatic/src/Processing.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 public class Processing {
 		private ArrayList<String> formatted; // keeps track of total output after loading multiple files
-		ArrayList<String> intermediate= new ArrayList<String>(); // List that keeps formatted lines of current loaded file. 
+		private ArrayList<String> intermediate= new ArrayList<String>(); // List that keeps formatted lines of current loaded file. 
 		//intermediate is needed in case the file being loaded has invalid flags somewhere in the file. Intermediate is added to 
 		//formatted once all the lines have been read in the file
 		
-		private char justification= 'l'; // 'l'for left, 'r' for right, and 'c' for center
-		private boolean single = true;  // true for single space, false for double spaced
-		private boolean title = false;  // true for centered title otherwise does nothing
-		private boolean indentation = false; // true for indentation otherwise does nothing
-		private boolean block = false; // true for block format otherwise does nothing
-		private boolean column1 = true; // true for one column, false for two columns
+		private char justification; // 'l'for left, 'r' for right, and 'c' for center
+		private boolean single;  // true for single space, false for double spaced
+		private boolean title;  // true for centered title otherwise does nothing
+		private boolean indentation; // true for indentation otherwise does nothing
+		private boolean block; // true for block format otherwise does nothing
+		private boolean column1;// true for one column, false for two columns
 		
 		
 		public Processing() {
@@ -23,6 +23,7 @@ public class Processing {
 			File file= new File(fileName);
 			BufferedReader reader= new BufferedReader(new FileReader(file));
 			ArrayList<String> linesToFormat = new ArrayList<String>();
+			setDefaults();
 			String str;
 			str = reader.readLine();
 			boolean firstFlag = true; // keeps track whether flag being read is the first flag in a formatting section
@@ -31,6 +32,8 @@ public class Processing {
 					if(str.charAt(0) == '-'){
 						if(firstFlag) {// if first flag of new formatting section, then format all the lines that were read
 							format(linesToFormat);
+							if(!single)
+								intermediate.add("");
 							linesToFormat.clear();
 							firstFlag= false;
 						}
@@ -65,24 +68,289 @@ public class Processing {
 			}
 			if(num == 2) { // if length ignoring white space is 2 then checks if given flag is part of valid list
 				char x= str.charAt(1);
-				if(x == 'r' || x == 'c' || x == 'l' || x == 't' || x== 'n' || x== 'd' || x== 's' || x == 'i' || x == 'b' || x == '2' || x == '1' || x == 'e') {
-					System.out.println("Valid Flag given.");
-					return true;
+				if(x == 'r' || x == 'c') {
+					indentation= false;
+					block= false;
+					justification = x;
+				}
+				else if(x == 'l') {
+					justification = x;
+				}
+				else if( x == 't') {
+					title = true;
+					indentation = false;
+					block = false;
+				}
+				else if(x == 'n' ) {
+					indentation = false;
+				}
+				else if(x == 'd') {
+					single = false;
+				}
+				else if(x == 's') {
+					single = true;
+				}
+				else if(x == 'i') {
+					indentation = true;
+					block = false;
+					justification = 'l';
+					column1 = true;
+					title = false;
+				}
+				else if(x == 'b') {
+					indentation = false;
+					block = true;
+					justification = 'l';
+					column1 = true;
+					title = false;
+				}
+				else if(x == '2') {
+					indentation = false;
+					block = false;
+					column1 = false;
+				}
+				else if(x == '1') {
+					column1 = true;
+				}
+				else if(x == 'e') {
+					if(!single) {
+						intermediate.add("");
+						intermediate.add("");
+					}
+					else
+						intermediate.add("");
 				}
 				else {
-					System.out.println("Invalid Flag given");
 					return false;
 				}
+				return true;
 			}
 			else {
-				System.out.println("Invalid Flag given");
-			}
 				return false;
+			}
+		}
+		
+		private void setDefaults() {
+			justification= 'l'; 
+			single = true;  
+			title = false; 
+			indentation = false; 
+			block = false; 
+			column1 = true;
 		}
 		
 		private void format(ArrayList<String> lines) {
-			// code for formatting based on flags
-			SampleFormat(lines);
+			if(title) {
+				formatTitle(lines.get(0));
+				if(!single)
+					intermediate.add("");
+				lines.remove(0);
+			}
+			if(indentation) {
+				formatIndent(lines);
+			}
+			else if(block) {
+				formatBlock(lines);
+			}
+			else if(justification == 'l') {
+				ArrayList<String> temp =leftJustify(lines);
+				if(column1) {
+					for(int i=0; i<temp.size(); i++)
+						intermediate.add(temp.get(i));
+				}
+				else
+					formatTwoColumns(temp);
+			}
+			else if(justification == 'r') {
+				ArrayList<String> temp =rightJustify(lines);
+				if(column1) {
+					for(int i=0; i<temp.size(); i++)
+						intermediate.add(temp.get(i));
+				}
+				else
+					formatTwoColumns(temp);
+			}
+			else if(justification == 'c') {
+				ArrayList<String> temp =centerJustify(lines);
+				if(column1) {
+					for(int i=0; i<temp.size(); i++)
+						intermediate.add(temp.get(i));
+				}
+				else
+					formatTwoColumns(temp);
+			}
+		}
+		
+		private void formatTitle(String line) {
+			String temp = line;
+			while(temp.length() <80) {
+				if(temp.length() == 79) {
+					temp= " "+ temp;
+				}
+				else {
+					temp = " "+ temp + " ";
+				}
+			}
+			intermediate.add(temp);
+		}
+		
+		private void formatIndent(ArrayList<String> lines) {
+			String temp= "";
+			for(int i=0; i<5; i++) {
+				temp= temp + " ";
+			}
+			for(int i=0; i< lines.size(); i++) {
+				String line= lines.get(i);
+				String [] words= line.split(" ");
+				for(int j=0; j< words.length; j++) {
+					String add= words[j];
+					if(temp.length() + add.length() +1 >80) {
+						intermediate.add(temp);
+						if(!single)
+							intermediate.add("");
+						temp= "";
+						temp= temp + add;
+					}
+					else {
+						if(temp.length() == 5) {
+							temp= temp + add;
+						}
+						else {
+						temp= temp +" "+ add;
+						}
+					}
+				}
+			}
+			if(temp.length()>0) {
+				intermediate.add(temp);
+			}
+		}
+		
+		private void formatBlock(ArrayList<String> lines) {
+			String temp= "";
+			for(int i=0; i<10; i++) {
+				temp= temp + " ";
+			}
+			for(int i=0; i< lines.size(); i++) {
+				String line= lines.get(i);
+				String [] words= line.split(" ");
+				for(int j=0; j< words.length; j++) {
+					String add= words[j];
+					if(temp.length() + add.length() +1 >80) {
+						intermediate.add(temp);
+						if(!single)
+							intermediate.add("");
+						temp= "";
+						for(int z=0; z<10; z++) {
+							temp= temp + " ";
+						}
+						temp= temp + add;
+					}
+					else {
+						if(temp.length() == 10) {
+							temp= temp + add;
+						}
+						else {
+						temp= temp +" "+ add;
+						}
+					}
+				}
+			}
+			if(temp.length()>0) {
+				intermediate.add(temp);
+			}
+		}
+		
+		private ArrayList<String> leftJustify(ArrayList<String> lines) {
+			ArrayList<String> list = new ArrayList<String>();
+			String temp= "";
+			int size;
+			if(column1) 
+				size = 80;
+			else
+				size = 35;
+			for(int i=0; i< lines.size(); i++) {
+				String line= lines.get(i);
+				String [] words= line.split(" ");
+				for(int j=0; j< words.length; j++) {
+					String add= words[j];
+					if(temp.length() + add.length() +1 >size) {
+						while(temp.length()<size) {
+							temp= temp+ " ";
+						}
+						list.add(temp);
+						if(!single && column1)
+							list.add("");
+						temp= "";
+						temp= temp + add;
+					}
+					else {
+						if(temp.length() == 0) {
+							temp= temp + add;
+						}
+						else {
+						temp= temp +" "+ add;
+						}
+					}
+				}
+			}
+			if(temp.length()>0) {
+				while(temp.length()<size)
+					temp= temp+ " ";
+				list.add(temp);
+			}
+			return list;
+		}
+		
+		private ArrayList<String> rightJustify(ArrayList<String> lines){
+			ArrayList<String> list = new ArrayList<String>();
+			String temp= "";
+			int size;
+			if(column1)
+				size = 80;
+			else
+				size = 35;
+			for(int i=0; i< lines.size(); i++) {
+				String line= lines.get(i);
+				String [] words= line.split(" ");
+				for(int j=0; j< words.length; j++) {
+					String add= words[j];
+					if(temp.length() + add.length() +1 >size) {
+						while(temp.length() <size) {
+							temp= " "+temp;
+						}
+						list.add(temp);
+						if(!single && column1)
+							list.add("");
+						temp= "";
+						temp= temp + add;
+					}
+					else {
+						if(temp.length() == 0) {
+							temp= temp + add;
+						}
+						else {
+						temp= temp +" "+ add;
+						}
+					}
+				}
+			}
+			if(temp.length()>0) {
+				while (temp.length() <size) {
+					temp= " " +temp;
+				}
+				list.add(temp);
+			}
+			return list;	
+		}
+		
+		private ArrayList<String> centerJustify(ArrayList<String> lines){
+			ArrayList<String> list = new ArrayList<String>();
+			return list;
+		}
+		
+		private void formatTwoColumns(ArrayList<String> lines) {
+			
 		}
 		
 		private void addToOutput() { // adds intermediate to the total output
@@ -103,191 +371,6 @@ public class Processing {
 				}
 			}
 			out.close();
-		}
-		
-		
-		
-		//code below is just sample implementation of various settings. Logic might work for actual implementation but this code
-		// does not work for actual implementation of the project as is.
-		private void SampleFormat(ArrayList<String> test) { //left justified 1 column
-			String temp= "";
-			for(int i=0; i< test.size(); i++) {
-				String line= test.get(i);
-				String [] words= line.split(" ");
-				for(int j=0; j< words.length; j++) {
-					String add= words[j];
-					if(temp.length() + add.length() +1 >80) {
-						intermediate.add(temp);
-						temp= "";
-						temp= temp + add;
-					}
-					else {
-						if(temp.length() == 0) {
-							temp= temp + add;
-						}
-						else {
-						temp= temp +" "+ add;
-						}
-					}
-				}
-			}
-			if(temp.length()>0)
-				intermediate.add(temp);
-		}
-		private void SampleFormat2(ArrayList<String> test) { // right justified 1 column
-			String temp= "";
-			for(int i=0; i< test.size(); i++) {
-				String line= test.get(i);
-				String [] words= line.split(" ");
-				for(int j=0; j< words.length; j++) {
-					String add= words[j];
-					if(temp.length() + add.length() +1 >80) {
-						while(temp.length() <80) {
-							temp= " "+temp;
-						}
-						intermediate.add(temp);
-						temp= "";
-						temp= temp + add;
-					}
-					else {
-						if(temp.length() == 0) {
-							temp= temp + add;
-						}
-						else {
-						temp= temp +" "+ add;
-						}
-					}
-				}
-			}
-			if(temp.length()>0)
-				while (temp.length() <80) {
-					temp= " " +temp;
-				}
-				intermediate.add(temp);
-		}
-		
-		private void SampleFormat3(ArrayList<String> test) { // block
-			String temp= "";
-			for(int i=0; i<10; i++) {
-				temp= temp + " ";
-			}
-			for(int i=0; i< test.size(); i++) {
-				String line= test.get(i);
-				String [] words= line.split(" ");
-				for(int j=0; j< words.length; j++) {
-					String add= words[j];
-					if(temp.length() + add.length() +1 >80) {
-						intermediate.add(temp);
-						temp= "";
-						for(int z=0; z<10; z++) {
-							temp= temp + " ";
-						}
-						temp= temp + add;
-					}
-					else {
-						if(temp.length() == 10) {
-							temp= temp + add;
-						}
-						else {
-						temp= temp +" "+ add;
-						}
-					}
-				}
-			}
-			if(temp.length()>0)
-				intermediate.add(temp);
+		}		
 }
-		private void SampleFormat4(ArrayList<String> test) { // indentation
-			String temp= "";
-			for(int i=0; i<5; i++) {
-				temp= temp + " ";
-			}
-			for(int i=0; i< test.size(); i++) {
-				String line= test.get(i);
-				String [] words= line.split(" ");
-				for(int j=0; j< words.length; j++) {
-					String add= words[j];
-					if(temp.length() + add.length() +1 >80) {
-						intermediate.add(temp);
-						temp= "";
-						temp= temp + add;
-					}
-					else {
-						if(temp.length() == 5) {
-							temp= temp + add;
-						}
-						else {
-						temp= temp +" "+ add;
-						}
-					}
-				}
-			}
-			if(temp.length()>0)
-				intermediate.add(temp);
-}
-		private void SampleFormat5(ArrayList<String> test) {// centered
-			String temp = test.get(0);
-			while(temp.length() <80) {
-				if(temp.length() == 79) {
-					temp= " "+ temp;
-				}
-				else {
-					temp = " "+ temp + " ";
-				}
-			}
-			intermediate.add(temp);
-			temp= "";
-			for(int i=1; i< test.size(); i++) {
-				String line= test.get(i);
-				String [] words= line.split(" ");
-				for(int j=0; j< words.length; j++) {
-					String add= words[j];
-					if(temp.length() + add.length() +1 >80) {
-						intermediate.add(temp);
-						temp= "";
-						temp= temp + add;
-					}
-					else {
-						if(temp.length() == 0) {
-							temp= temp + add;
-						}
-						else {
-						temp= temp +" "+ add;
-						}
-					}
-				}
-			}
-			if(temp.length()>0)
-				intermediate.add(temp);
-		}
-		
-		private void SampleFormat6(ArrayList<String> test) {// individual line of 2 column
-			String temp= "";
-			for(int i=0; i< test.size(); i++) {
-				String line= test.get(i);
-				String [] words= line.split(" ");
-				for(int j=0; j< words.length; j++) {
-					String add= words[j];
-					if(temp.length() + add.length() +1 >35) {
-						while(temp.length() <35) {
-							temp= temp + " "; 
-						}
-						intermediate.add(temp);
-						temp= "";
-						temp= temp + add;
-					}
-					else {
-						if(temp.length() == 0) {
-							temp= temp + add;
-						}
-						else {
-						temp= temp +" "+ add;
-						}
-					}
-				}
-			}
-			if(temp.length()>0)
-				intermediate.add(temp);
-		}
-		
-}
+
